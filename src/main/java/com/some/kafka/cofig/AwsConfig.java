a6dce0ad096fa36ff3c1614ce0b6205a125e7153
Stanislav Zhuravel <szhurav@softserveinc.com>
2019-05-23 21:13:57 +0300

    dynamoDB settings

---
+++ src/main/java/com/some/kafka/cofig/AwsConfig.java
@@ -0,0 +1,28 @@
+package com.some.kafka.cofig;
+
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AwsConfig {
+
+    @Bean
+    public AmazonDynamoDB dynamoDB(@Value("${storage.s3.region}") String region, AWSCredentialsProvider credentials) {
+        return  AmazonDynamoDBClientBuilder.standard()
+            .withRegion(region)
+            .withCredentials(credentials)
+            .build();
+    }
+
+    @Bean
+    public AWSCredentialsProvider awsCredentials(@Value("${storage.s3.key}") String key, @Value("${storage.s3.secret}") String secret) {
+        return new AWSStaticCredentialsProvider(new BasicAWSCredentials(key, secret));
+    }
+
+}
--- src/main/java/com/some/kafka/service/kafka/TemperatureWorker.java
+++ src/main/java/com/some/kafka/service/kafka/TemperatureWorker.java
@@ -7,20 +7,23 @@ import com.some.kafka.service.FireService;
 import com.some.kafka.service.TemperatureService;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.kstream.Joined;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Materialized;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.time.Clock;
 
 import static com.some.kafka.cofig.KafkaConfig.KafkaTopicsProperties.KafkaTopicProperties;
+
 
 @Slf4j
 @Service
@@ -32,6 +35,8 @@ public class TemperatureWorker extends KafkaWorker {
     private final TemperatureService temperatureService;
     private final FireService fireService;
     private final Clock clock;
+    private Serde<FireEvent> fireEventSerde;
+    private Serde<String> stringSerde;
 
     @Autowired
     public TemperatureWorker(@NonNull KafkaProperties kafkaProperties,
@@ -47,53 +52,46 @@ public class TemperatureWorker extends KafkaWorker {
         this.clock = clock;
     }
 
+    @PostConstruct
+    private void initTemperatureWorker() {
+        this.fireEventSerde = fireTopic.getValueSerde();
+        this.stringSerde = Serdes.String();
+    }
+
     @Override
     protected void createTopology(@NonNull StreamsBuilder builder) {
-        var temperatureTable = temperatureTable(builder);
-
-        temperatureTable.toStream().foreach((k,e) -> checkFireState());
-        var fireTable = fireTable(builder);
-        fireStream(temperatureStream, fireTable).groupByKey().reduce((oldValue, newValue) -> newValue).toStream().foreach(((key, value) -> System.out.println(key =" =++++++++> " + value)));
+        var temperatureStream = temperatureStream(builder);
+        fireTable(temperatureStream).toStream().foreach((key, value) -> System.out.println(value));
     }
 
-
-    private KTable<String, com.some.kafka.model.temperature.TemperatureEvent> temperatureTable(StreamsBuilder builder) {
-        return builder.table(this.temperatureTopic.getName(), this.temperatureTopic.consumed());
+    private KStream<String, com.some.kafka.model.temperature.TemperatureEvent> temperatureStream(StreamsBuilder builder) {
+        return builder.stream(this.temperatureTopic.getName(), this.temperatureTopic.consumed());
     }
 
-
-    private KStream<String, FireEvent> fireStream(KTable<String, com.some.kafka.model.temperature.TemperatureEvent> temperatureTable,KTable<String, FireEvent> fireTable) {
-        return temperatureTable.toStream().leftJoin(fireTable,
-            (leftValue, rightValue) -> updateFireState(leftValue, rightValue),
-            Joined.keySerde(this.temperatureTopic.getKeySerde().serializer())
-                .withValueSerde(fireTopic.getValueSerde())); /* key */
+    private KTable<String, FireEvent> fireTable(KStream<String, TemperatureEvent> temperatureStream) {
+        return temperatureStream.groupByKey()
+            .aggregate(FireEvent::getDefaultInstance, this::updateFireState, Materialized.as("fire-store").with(stringSerde, fireEventSerde));
     }
 
-    private FireEvent updateFireState(TemperatureEvent temperatureEvent, FireEvent fireEvent) {
-        Integer temeperature = temperatureEvent.getTemperatureUpserted().getTemperature().getValue();
+    private FireEvent updateFireState(String id, TemperatureEvent newTemperatureEvent, FireEvent aggregated) {
+        Integer newTemperature = newTemperatureEvent.getTemperatureUpserted().getTemperature().getValue();
 
-        if (fireEvent.hasFireStarted() && temeperature < 55) {
-            return temperatureToFireStopped(temperatureEvent);
+        if (aggregated.hasFireStarted() && newTemperature <= 55) {
+            return temperatureToFireStopped(newTemperatureEvent);
         }
-        if (temeperature > 55) {
-            return temperatureToFireStarted(temperatureEvent);
-
+        if (newTemperature > 55) {
+            return temperatureToFireStarted(newTemperatureEvent);
         }
-        if (temeperature >= 37) {
-            return temperatureToFireWarning(temperatureEvent);
-        }
-
-        return fireEvent;
+        return temperatureToFireWarning(newTemperatureEvent);
     }
-
 
     public com.some.kafka.model.fire.FireEvent temperatureToFireStarted(com.some.kafka.model.temperature.TemperatureEvent temperatureEvent) {
 
         com.some.kafka.model.fire.FireStarted fireStarted = com.some.kafka.model.fire.FireStarted.newBuilder().build();
         var event = com.some.kafka.model.fire.FireEvent.
-                newBuilder().
-                setCreatedBy(temperatureEvent.getCreatedBy()).
-                setCreatedAt(temperatureEvent.getCreatedAt()).setId(temperatureEvent.getId()).setFireStarted(fireStarted).build();
+            newBuilder().
+            setCreatedBy(temperatureEvent.getCreatedBy()).
+            setCreatedAt(temperatureEvent.getCreatedAt()).setId(temperatureEvent.getId()).setFireStarted(fireStarted).build();
 
         return event;
     }
@@ -103,9 +101,9 @@ public class TemperatureWorker extends KafkaWorker {
         com.some.kafka.model.fire.FireWarning fireWarning = com.some.kafka.model.fire.FireWarning.newBuilder().build();
 
         var event = com.some.kafka.model.fire.FireEvent.
-                newBuilder().
-                setCreatedBy(temperatureEvent.getCreatedBy()).
-                setCreatedAt(temperatureEvent.getCreatedAt()).setId(temperatureEvent.getId()).setFireWarning(fireWarning).build();
+            newBuilder().
+            setCreatedBy(temperatureEvent.getCreatedBy()).
+            setCreatedAt(temperatureEvent.getCreatedAt()).setId(temperatureEvent.getId()).setFireWarning(fireWarning).build();
 
         return event;
     }
@@ -115,9 +113,9 @@ public class TemperatureWorker extends KafkaWorker {
         com.some.kafka.model.fire.FireStopped fireStopped = com.some.kafka.model.fire.FireStopped.newBuilder().build();
 
         var event = com.some.kafka.model.fire.FireEvent.
-                newBuilder().
-                setCreatedBy(temperatureEvent.getCreatedBy()).
-                setCreatedAt(temperatureEvent.getCreatedAt()).setId(temperatureEvent.getId()).setFireStopped(fireStopped).build();
+            newBuilder().
+            setCreatedBy(temperatureEvent.getCreatedBy()).
+            setCreatedAt(temperatureEvent.getCreatedAt()).setId(temperatureEvent.getId()).setFireStopped(fireStopped).build();
 
         return event;
     }
